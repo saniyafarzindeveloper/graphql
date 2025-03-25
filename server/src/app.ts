4213754faf6b89ccac3133cff375bcbeadd8c1a7
saniyafarzindeveloper <developerfarzin@gmail.com>
2025-03-25 21:03:27 +0530

    cleaning up

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,10 +17,7 @@ console.log("Connecting to:", mongoURI);
 connectDB(mongoURI);
 
 
-// export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
-// const port = Number(process.env.PORT) || 3000;
-// const mongoURI = process.env.MONGO_URI!;
-// connectDB(mongoURI);
+
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
